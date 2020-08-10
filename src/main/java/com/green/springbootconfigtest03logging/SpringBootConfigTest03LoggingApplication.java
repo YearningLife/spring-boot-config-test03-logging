b4baf9b8541c92345682bc1bfe8677307aeaf39d
齐鹏飞 <qipengfly@qq.com>
2020-08-10 20:12:42 +0800

    SpringBootConfig配置学习

---
+++ src/main/java/com/green/springbootconfigtest03logging/SpringBootConfigTest03LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.green.springbootconfigtest03logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigTest03LoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigTest03LoggingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/springbootconfigtest03logging/SpringBootConfigTest03LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.springbootconfigtest03logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigTest03LoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
